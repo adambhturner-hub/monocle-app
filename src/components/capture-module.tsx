2e4456755b848d4659c9bdfab8519173cbddf3fb
Adam Turner <adamturner@Adams-MacBook-Pro.local>
2026-03-18 09:47:55 -0400

    fix: Build full SpeechRecognition transcript instead of slicing on pause

--- src/components/capture-module.tsx
+++ src/components/capture-module.tsx
@@ -127,7 +127,7 @@ export function CaptureModule({ taskToEdit, onComplete, isModal = false }: Captu
                 let interimTranscript = '';
                 let finalTranscript = '';
 
-                for (let i = event.resultIndex; i < event.results.length; ++i) {
+                for (let i = 0; i < event.results.length; ++i) {
                     if (event.results[i].isFinal) {
                         finalTranscript += event.results[i][0].transcript;
                     } else {
